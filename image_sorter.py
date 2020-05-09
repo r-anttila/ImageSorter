46797a79f081f3ede0e473ab29ce73925a8a0c8f
ranttila <ranttila97@gmail.com>
2020-05-09 11:40:28 +0300

    Updated README and cleaned up code

--- image_sorter.py
+++ image_sorter.py
@@ -1,4 +1,4 @@
-import FaceDetection.face_detection as fd
+import face_detection as fd
 import os
 import pathlib as path
 import cv2 as cv
@@ -18,13 +18,13 @@ def create_output_folder(p):
 # Create an argument parser to read input from command line
 parser = argparse.ArgumentParser(
     description='A python script that sorts images from a folder to two folders, one containing photos with faces and other those without faces')
-parser.add_argument(
-    '--in_folder', help="The path to the folder you want to sort. Default is current folder.", default=path.Path())
+parser.add_argument('--in_folder', help="The path to the folder you want to sort. Default is current folder.",
+                    default=path.Path())
 parser.add_argument('--faces_folder', help="The path to the folder where the images with faces are stored. Default is in_folder/faces.",
                     default=path.Path('faces'))
 parser.add_argument('--no_faces_folder', help="The path to the folder where the images without faces are stored. Default is in_folder/no_faces.",
                     default=path.Path('no_faces'))
-parser.add_argument('--cv_folder', help='The path to the folder containing the cv2 installation',
+parser.add_argument('--cv_path', help='The path to the folder containing the cv2 installation',
                     default="C:/Users/Roope/miniconda3/envs/tf/Lib/site-packages/cv2")
 args = parser.parse_args()
 
@@ -45,9 +45,9 @@ images = os.scandir(in_folder)
 
 # Getting the profile and frontal cascades from the cv2 library
 profileface_cascade = os.path.join(
-    args.cv_folder, "data\haarcascade_profileface.xml")
+    args.cv_path, "data\haarcascade_profileface.xml")
 frontalface_cascade = os.path.join(
-    args.cv_folder, "data\haarcascade_frontalface_alt.xml")
+    args.cv_path, "data\haarcascade_frontalface_alt.xml")
 
 # Creating two face detectors, one for detecting full frontal faces and one for profiles
 # For best result we combine results from both detectors to check for faces
@@ -58,6 +58,10 @@ profile_detector = fd.FaceDetector(profileface_cascade)
 print("Identifying images...")
 images = [image for image in images if image.is_file()
           and ft.is_image(image.path)]
+
+if not images:  # Exit the program if no images are found
+    print("No Images found in the given folder.")
+    exit()
 
 for image in tqdm(images, desc="Sorting pictures"):
     try:
